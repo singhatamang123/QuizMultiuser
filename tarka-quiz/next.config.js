dadfafc77e6e0264d489f49f937200822230e5f9
singhatamang123 <singhatamang123@gmail.com>
2026-04-18 12:20:01 +0545

    fix vercel deployment

--- tarka-quiz/next.config.js
+++ tarka-quiz/next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/QuizMultiuser' : '',
-  assetPrefix: isProd ? '/QuizMultiuser/' : '',
-  trailingSlash: true,
 };
 
 module.exports = nextConfig;
