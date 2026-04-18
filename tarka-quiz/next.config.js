889a32c9202232cfd579af88b3742ad070bb847e
singhatamang123 <singhatamang123@gmail.com>
2026-04-18 12:17:03 +0545

    ready for vercel deployment

--- tarka-quiz/next.config.js
+++ tarka-quiz/next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/QuizMultiuser',
-  assetPrefix: '/QuizMultiuser/',
+  basePath: isProd ? '/QuizMultiuser' : '',
+  assetPrefix: isProd ? '/QuizMultiuser/' : '',
   trailingSlash: true,
 };
 
